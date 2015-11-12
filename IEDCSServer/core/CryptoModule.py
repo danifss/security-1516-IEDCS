0546e9740b8982228c6ee789394ea9ef8cfe6487
Tiago Oliveira <tiago9@ua.pt>
2015-11-12 17:50:11 +0000

    RSA changes , new functions

--- IEDCSServer/core/CryptoModule.py
+++ IEDCSServer/core/CryptoModule.py
@@ -5,26 +5,105 @@ from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 import Crypto.Random as Random
 import hashlib
+from base64 import b64decode
 
 
 
 class CryptoModule(object):
-    # cypher_text = ""
-    # plain_text = ""
-    rsaKeyPub = ""
-    rsaKeyPriv = ""
+
+    def __init__(self):
+        self.rsaKeyPriv = ""
+        self.rsaKeyPub = ""
+        # cypher_text = ""
+        # plain_text = ""
 
 
     """
-        RSA encrypt
+        RSA generate
     """
-    def rsaKeys(self, data):
-        rsaKey = RSA.generate(2048)
+    # returns rsa key object
+    def generateRsa(self):
 
-        # self.rsaKeyPriv = rsaKey.has_private()
-        # rsaKeyPub = rsaKey.publickey()
-        # encrypted = rsaKey.encrypt(data, 32)
-        #return rsaKey
+        pairKey = RSA.generate(2048)
+
+        return pairKey
+
+    """
+        RSA public key
+    """
+    # returns public key object
+    def publicRsa(self, pairKey):
+
+        try:
+            pairKey.can_encrypt()
+        except Exception:
+            print "Key not valid"
+
+        return pairKey.publickey()
+
+    """
+        RSA public and private
+    """
+    #
+    def pairRsa(self, pairKey):
+
+        try:
+            pairKey.can_encrypt()
+            # check if is pair and not only public key
+            pairKey.has_private()
+        except Exception:
+            print "Key not valid"
+            return 1
+
+        # Construct a new key carrying only the public information.
+        # public = rsaKey.publickey()
+        # Retrives data of the public key, format PEM
+        # pub = public.exportKey()
+
+        # Retrives data of the public key, format PEM
+        pub = pairKey.publickey().exportKey('PEM')
+        # Retrives data of the private key, format PEM
+        priv = pairKey.exportKey('PEM')
+
+        # # cifrar directamente com a chave publica, e decifrar com a chave rsa
+        # x = pairKey.publickey()
+        # encry = x.encrypt('encripta tudo',32)
+        # dec = pairKey.decrypt(encry)
+        # print encry
+        # print dec
+
+        return pub, priv
+
+    """
+        RSA ciphering
+    """
+    # receives rsa key, returns ciphered text
+    def rsaCipher(self, pairKey, data):
+
+        # import from file
+        # pairKey = RSA.importKey(key)
+        cipher = PKCS1_OAEP.new(pairKey)
+        ciphertext = cipher.encrypt(data)
+
+        # base64 turns the symbols of the ciphered text to "readable letters"
+        return ciphertext.encode('base64')
+
+    """
+        RSA DEciphering
+    """
+    def rsaDecipher(self, pairKey, data):
+
+        try:
+            pairKey.can_encrypt()
+            # check if is pair and not only public key
+            pairKey.has_private()
+        except Exception:
+            print "Key not valid"
+            return 1
+
+        cipher = PKCS1_OAEP.new(pairKey)
+        # if ciphertext was produced with base64, reverve using b64decode
+        return cipher.decrypt(b64decode(data))
 
 
     """
@@ -35,6 +114,8 @@ class CryptoModule(object):
         encryption_suite = AES.new(key, AES.MODE_CFB, vi)
         # encrypt
         cipher_text = encryption_suite.encrypt(data)
+
+
         return cipher_text
 
 
@@ -62,21 +143,32 @@ class CryptoModule(object):
         return hex_dig
 
 
+f = CryptoModule()
 
-
-# f = CryptoModule()
 
 ### Testing RSA
-# f.rsaKeys("a")
+key = f.generateRsa()
 
+
+data = 'bamos a esto'
+cifrado = f.rsaCipher(key,data)
+print cifrado
+
+key2 = f.generateRsa()
+
+wrongkey='dsad'
+clear = f.rsaDecipher(wrongkey, cifrado)
+print clear
 
 ### Testing SHA256
 # print f.hashingSHA256("ola mundo")
 
-### Testing AES
-vi = "jugujugleswagger"
-#    cipherAES(key, vi, data)
-c = f.cipherAES("_swaggerswagger_", vi, "isto e um espetaculo")
-print "Cifra: " + c
-p = f.decipherAES("_swaggerswagger_", vi, c)
-print "Plain text: " + p
+# ### Testing AES
+# vi = "jugujugleswagger"
+# # random vi
+# # vi = Random.new().read(AES.block_size)
+# #    cipherAES(key, vi, data)
+# c = f.cipherAES("_swaggerswagger_", vi, "isto e um espetaculo")
+# print "Cifra: " + c
+# p = f.decipherAES("_swaggerswagger_", vi, c)
+# print "Plain text: " + p
