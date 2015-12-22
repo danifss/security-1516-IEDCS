55f175510201f4d2142321699ca2926fa4af2e6a
danifss <daniel.silva@ua.pt>
2015-12-22 11:11:55 +0000

    Added challenge api path on resources file.

--- IEDCSPlayer/Resources.py
+++ IEDCSPlayer/Resources.py
@@ -11,6 +11,7 @@ class api:
     GET_CONTENT_BY_USER = serverUrl + "/api/content/user/"
     GET_CONTENT_TO_PLAY = serverUrl + "/api/content/play/"
     GET_CONTENT_PAGES = serverUrl + "/api/content/pages/"
+    CHALLENGE = serverUrl + "/api/content/challenge/"
     HAS_CONTENT_TO_PLAY = serverUrl + "/api/content/hascontent/"
     # GET_CONTENT_FILENAME = serverUrl + "/api/content/name/"
     # GET_CONTENT_FILEPATH = serverUrl + "/api/content/filepath/"
