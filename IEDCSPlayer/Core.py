39c587bb221d0c2c78962a460c7679fdb158252a
danifss <daniel.silva@ua.pt>
2015-12-28 19:03:59 +0000

    Fixed bug with iv.

--- IEDCSPlayer/Core.py
+++ IEDCSPlayer/Core.py
@@ -87,8 +87,9 @@ class Core(object):
                 return
             res = json.loads(result.text)
             iv = res["iv"]
+            iv_raw = iv.decode('base64')
             
-            decipheredFile = self.crypt.decipherAES('uBAcxUXs1tJYAFSI', iv, f.read())
+            decipheredFile = self.crypt.decipherAES('uBAcxUXs1tJYAFSI', iv_raw, f.read())
             f.close()
             src = StringIO(decipheredFile)
 
@@ -137,8 +138,9 @@ class Core(object):
                 return
             res = json.loads(result.text)
             iv = res["iv"]
+            iv_raw = iv.decode('base64')
 
-            player = self.crypt.decipherAES("vp71cNkWdASAPXp4", iv, playerPublic)
+            player = self.crypt.decipherAES("vp71cNkWdASAPXp4", iv_raw, playerPublic)
             #print "Player Public Key", player
             self.playerKey = self.crypt.rsaImport(player)
             #self.playerHash = CryptoModule.hashingSHA256(player)
