d1850e858909116f005636080549e3b5010f8c04
danifss <daniel.silva@ua.pt>
2015-12-29 01:52:16 +0000

    Regex validation for password: ((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%_-]).{8,16})

--- IEDCSServer/core/forms.py
+++ IEDCSServer/core/forms.py
@@ -1,20 +1,24 @@
 from django import forms
 from django.forms import ModelForm
 from .models import User
+from django.core.validators import RegexValidator
 
 
 class registerUserForm(ModelForm):
-    # password = forms.CharField(label="Password", widget=forms.PasswordInput, help_text="Enter password")
-    # username = forms.CharField(label="Username", max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Username'}),)
-    # password = forms.CharField(widget=forms.PasswordInput, required=True,)
-    # email = forms.EmailField(label="Email", max_length=150, required=True, widget=forms.TextInput(attrs={'placeholder': 'Email'}),)
-    # first_name = forms.CharField(label="First Name", max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'First Name'}),)
-    # last_name = forms.CharField(label="Last Name", max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Last Name'}),)
+    username = forms.CharField(label="Username", max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Username'}),)
+    # password = forms.CharField(label="Password", required=True, widget=forms.PasswordInput, help_text="Enter password")
+    userCC = forms.CharField(label="CC Number", min_length=8, max_length=8, required=True, widget=forms.TextInput(attrs={'placeholder': 'Citizen Card Number'}),)
+    password = forms.CharField(widget=forms.PasswordInput, min_length=8, max_length=16, required=True, \
+                        validators=[RegexValidator(regex='^.((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%_-]).{8,16})$', \
+                        message='Password needs to have [a-zA-Z0-9]+ and at least one of this characters [@#$%-_]', code='nomatch')])
+    email = forms.EmailField(label="Email", max_length=150, required=True, widget=forms.TextInput(attrs={'placeholder': 'Email'}),)
+    firstName = forms.CharField(label="First Name", max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'First Name'}),)
+    lastName = forms.CharField(label="Last Name", max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Last Name'}),)
 
     class Meta:
         model = User
         fields = ['username', 'password', 'userCC', 'email', 'firstName', 'lastName']
-        widgets = { 'password' : forms.PasswordInput() }
+        # widgets = { 'password' : forms.PasswordInput() }
 
 
 class loginForm(forms.Form):
